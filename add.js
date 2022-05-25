d5b56d9f9686df634f5a1b5d0c618ded87e62f37
Pratik sakhare <pratik.sakhare@delhivery.com>
2022-05-25 19:33:28 +0530

    cleaning code
    divided logic into separate smaller funtions
    divided test cases to describe blocks

--- add.js
+++ add.js
@@ -1,35 +1,57 @@
 const getNumbersFromString = (numberString) => {
 
+    if(!numberString) {
+        return [];
+    }
     const numberRegEx = /-?\d+/g;
     var numbers = numberString.match(numberRegEx).map(Number);
     return numbers;
 }
 
-let addNumbersFromNumberString = (numbers) => {
-    //Handling Empty numberString 
-    if (!numbers) {
-        return 0;   
+const validateInput = (numberString) => {
+
+    if (!numberString) {
+        return false;   
     }
-    let numbersList = getNumbersFromString(numbers);
 
-    //Handling single numberString
-    if (numbersList.length == 1)
-        throw new Error('Input is NOT ok')
+    let numbersList = getNumbersFromString(numberString);
+    
+    if (numbersList.length <= 1)
+        throw new Error('Input is NOT ok');
 
-    let sum = 0;
-    let negativeNumbers = []
-    numbersList.forEach(n => {
-        if(n < 0) {
-            negativeNumbers.push(n)
+    let negativeNumbers = [];
+    numbersList.forEach(number => {
+        if(number < 0) {
+            negativeNumbers.push(number)
         }
-        sum += n;
     });
 
-    //Handling negative numbers
     if (negativeNumbers.length) {
-        throw new Error(`negatives not allowed ${negatives.join(',')}`);
+        throw new Error(`negatives not allowed ${negativeNumbers.join(',')}`);
     }
-    return sum;
+    return true;
+}
+
+const addNumbersFromNumberString = (numberString) => {
+
+    try {
+        console.log(`Input : ${numberString}`);
+
+        let isValidInput = validateInput(numberString);
+        if (!isValidInput) {
+            return 0;
+        }
+
+        let numbersList = getNumbersFromString(numberString);
+        let sum = numbersList.reduce((total, number) => total + number, 0);
+
+        console.log(`Output : ${sum}`);
+        return sum;
+        
+    } catch (exception) {
+        console.log(`Error : ${exception}`);
+        return exception.message;
+    }
 }
 
 module.exports = addNumbersFromNumberString
--- add.spec.js
+++ add.spec.js
@@ -1,70 +1,50 @@
 const add = require('./add');
 
-describe('testing exceptional Input', () => {
-    test('empty Numbers string is not allowed', () => {
+describe('testing exceptional input', () => {
+    test('empty numbers string is not allowed', () => {
         const input = "";
-        const output = add("");
-        console.log(`Input : ${input} Output : ${output}`);
+        const output = add(input);
         expect(output).toBe(0);
     });
 
     test('single number string is not allowed', () => {
         const input = "1";
-        let output;
-        try {
-            add(input);
-        } catch (e) {
-            output = e.message;
-        }
-        console.log(`Input : ${input} Output : ${output}`);
+        let output = add(input);
         expect(output).toBe('Input is NOT ok');
     });
 });
 
-describe('testing Input with negative numbers', () => {
+describe('testing input with negative numbers', () => {
+
     test('negative number is not allowed', () => {
         const input = "//;\n-1;2";
-        let output;
-        try {
-            add(input);
-        } catch (e) {
-            output = e.message;
-        }
-        console.log(`Input : ${input} Output : ${output}`);
+        let output = add(input);
         expect(output).toBe('negatives not allowed -1');
     });
+
     test('multiple negative numbers are not allowed', () => {
         const input = "//;\n-1;-2//-3";
-        let output;
-        try {
-            add(input);
-        } catch (e) {
-            output = e.message;
-        }
-        console.log(`Input : ${input} Output : ${output}`);
+        let output = add(input);
         expect(output).toBe('negatives not allowed -1,-2,-3');
     });
 });
 
-describe('testing diffrent delimiters numberString', () => {
-    test('two numbers, separated by commas', () => {
+describe('testing input with diffrent delimiters', () => {
+    test('two numbers separated by commas', () => {
         const input = "1,2";
         const output = add(input);
-        console.log(`Input : ${input} Output : ${output}`);
         expect(output).toBe(3);
     });
     
-    test('unknown count of numbers in numberString', () => {
+    test('three numbers separated by different delimiters', () => {
         const input = "1\n2,3";
         const output = add(input);
-        console.log(`Input : ${input} Output : ${output}`);
         expect(output).toBe(6);
     });
     
-    test('different list of delimiters numberString', () => {
+    test('numbers with different group of delimiters', () => {
         const input = "//;\n1;2";
         const output = add(input);
-        console.log(`Input : ${input} Output : ${output}`);
         expect(output).toBe(3);
     });
 });
